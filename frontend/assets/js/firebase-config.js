be3f7a82443c0ee3260cdcb39fbcef1b54e9746c
devjypadyan <devjypadyan@gmail.com>
2024-07-29 01:14:19 +0530

    google login module

---
+++ frontend/assets/js/firebase-config.js
@@ -0,0 +1,14 @@
+//configuration for firebase-app
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYM83wOxI6UtopHKXTIgIBNqFMNPoerTM",
+  authDomain: "trial-d8961.firebaseapp.com",
+  databaseURL: "https://trial-d8961-default-rtdb.firebaseio.com",
+  projectId: "trial-d8961",
+  storageBucket: "trial-d8961.appspot.com",
+  messagingSenderId: "873095144159",
+  appId: "1:873095144159:web:1697300ca6b50518b9ff6a",
+  measurementId: "G-DQPDQGLFNE"
+};
+
+export { firebaseConfig };
---
+++ frontend/assets/js/user-details.js
@@ -0,0 +1,20 @@
+/**
+ * User-details.js
+ * Script file used to fetch the user details from localstorage
+ * convert it as userDetailsObj and export it
+ */
+let userDetails = localStorage.getItem("userDetails");
+let userDetailsArray = JSON.parse(userDetails);
+const userDeatilObj = {
+    name: userDetailsArray[0],
+    email: userDetailsArray[1],
+    phoneNumber: userDetailsArray[2],
+    photoURL: userDetailsArray[3],
+    accessToken: userDetailsArray[4]
+}
+
+//setting user-name data on the header section near profile menu "Hi, "
+document.getElementById("user-name").innerHTML = userDeatilObj.name;
+document.getElementById("user-name2").innerHTML = userDeatilObj.name;
+
+export { userDeatilObj }
