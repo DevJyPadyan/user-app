f529918c12a72daece678caf961a12e7605b6eb5
devjypadyan <devjypadyan@gmail.com>
2024-07-31 00:49:11 +0530

    Api key addition, Login/Logout Functionalities, User name display

--- frontend/assets/js/loader.js
+++ frontend/assets/js/loader.js
@@ -7,8 +7,10 @@ import { userDeatilObj } from "./user-details.js";
 setTimeout(() => {
   const loader = document.querySelector(".skeleton-loader");
   loader.style.display = "none";
-  
+
   //setting user-name data on the header section near profile menu "Hi, "
-  document.getElementById("user-name").innerHTML = userDeatilObj.name;
-  document.getElementById("user-name2").innerHTML = userDeatilObj.name;
+  document.getElementById("user-name").innerHTML = userDeatilObj.name == undefined ? "User" : userDeatilObj.name;
+  if (document.getElementById("user-name2") != null) {
+    document.getElementById("user-name2").innerHTML = userDeatilObj.name == undefined ? "User" : userDeatilObj.name;
+  }
 }, 4000);
---
+++ frontend/assets/js/logout.js
@@ -0,0 +1,18 @@
+/**
+ * logout.js will be executed, when user clicks on logout
+ */
+
+logoutBtn.addEventListener('click',(e)=>{
+    //removing user-details from the localstorage, once logout is clicked.
+    localStorage.removeItem("userDetails");
+    window.location.href='././signin.html';
+});
+
+//logout button inside Logout Modal.
+if(document.getElementById("modalLogoutBtn") != null ){
+    modalLogoutBtn.addEventListener('click',(e)=>{
+        //removing user-details from the localstorage, once logout inside Modal Logout is clicked.
+        localStorage.removeItem("userDetails");
+        window.location.href='././signin.html';
+    });
+}
